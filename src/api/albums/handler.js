02706ad9d732d2bb59a766c1f865aacb5b481115
Thariq <thariq2712@gmail.com>
2024-08-07 00:31:34 +0700

    implement validator and extends error handling

--- src/api/albums/handler.js
+++ src/api/albums/handler.js
@@ -1,13 +1,15 @@
 const autoBind = require('auto-bind');
 
 class AlbumsHandler {
-  constructor(service) {
+  constructor(service, validator) {
     this._service = service;
+    this._validator = validator;
 
     autoBind(this);
   }
 
   async postAlbumHandler(request, h) {
+    this._validator.validateAlbumPayload(request.payload);
     const { name, year } = request.payload;
     const albumId = await this._service.addAlbum({ name, year });
 
@@ -35,6 +37,7 @@ class AlbumsHandler {
   }
 
   async putAlbumByIdHandler(request) {
+    this._validator.validateAlbumPayload(request.payload);
     const { id } = request.params;
     await this._service.editAlbumById(id, request.payload);
 
--- src/server.js
+++ src/server.js
@@ -3,8 +3,10 @@ require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 
 const ClientError = require('./exceptions/ClientError');
+
 const albums = require('./api/albums');
 const AlbumsService = require('./services/postgres/AlbumsService');
+const AlbumsValidator = require('./validator/albums');
 
 const init = async () => {
   const albumsService = new AlbumsService();
@@ -22,24 +24,38 @@ const init = async () => {
     plugin: albums,
     options: {
       service: albumsService,
+      validator: AlbumsValidator,
     },
   }]);
 
   server.ext('onPreResponse', (request, h) => {
     const { response } = request;
 
-    if (response instanceof ClientError) {
+    if (response instanceof Error) {
+
+      if (response instanceof ClientError) {
+        const newResponse = h.response({
+          status: 'fail',
+          message: response.message,
+        });
+        newResponse.code(response.statusCode);
+        return newResponse;
+      }
+
+      if (!response.isServer) {
+        return h.continue;
+      }
+
       const newResponse = h.response({
-        status: 'fail',
-        message: response.message,
+        status: 'error',
+        message: 'terjadi kegagalan pada server kami',
       });
-      newResponse.code(response.statusCode);
+      newResponse.code(500);
       return newResponse;
     }
 
     return h.continue;
   });
-
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
