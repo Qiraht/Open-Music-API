28464718cc4a552bcfaf37fcd0386eec22ca2f04
Thariq <thariq2712@gmail.com>
2024-08-07 17:06:15 +0700

    add utils to map songs db to model

---
+++ src/utils/songs/index.js
@@ -0,0 +1,19 @@
+const mapSongsDBToModel = ({
+  id,
+  title,
+  year,
+  performer,
+  genre,
+  duration,
+  album_id,
+}) => ({
+  id,
+  title,
+  year,
+  genre,
+  performer,
+  duration,
+  albumId: album_id,
+});
+
+module.exports = mapSongsDBToModel;
