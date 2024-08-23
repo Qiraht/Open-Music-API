2bc9fd0266646cc7be66072d65df569ae0ae68d4
Thariq <thariq2712@gmail.com>
2024-08-23 22:28:46 +0700

    register users plugin

--- src/server.js
+++ src/server.js
@@ -11,10 +11,15 @@ const AlbumsValidator = require('./validator/albums');
 const songs = require('./api/songs');
 const SongsService = require('./services/postgres/SongsService');
 const SongsValidator = require('./validator/songs');
+const UsersService = require('./services/postgres/UsersService');
+const users = require('./api/users');
+const UsersValidator = require('./validator/users');
 
 const init = async () => {
   const albumsService = new AlbumsService();
   const songsService = new SongsService();
+  const usersService = new UsersService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -25,20 +30,29 @@ const init = async () => {
     },
   });
 
-  await server.register([{
-    plugin: albums,
-    options: {
-      service: albumsService,
-      validator: AlbumsValidator,
+  await server.register([
+    {
+      plugin: albums,
+      options: {
+        service: albumsService,
+        validator: AlbumsValidator,
+      },
     },
-  },
-  {
-    plugin: songs,
-    options: {
-      service: songsService,
-      validator: SongsValidator,
+    {
+      plugin: songs,
+      options: {
+        service: songsService,
+        validator: SongsValidator,
+      },
+    },
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      }
     }
-  }]);
+  ]);
 
   server.ext('onPreResponse', (request, h) => {
     const { response } = request;
