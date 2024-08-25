5ab4554c0146360127342a63205222ea08d7ada7
Thariq <thariq2712@gmail.com>
2024-08-26 00:42:47 +0700

    add migrations for playlists and playlist songs

---
+++ migrations/1724431564659_create-table-playlist-songs.js
@@ -0,0 +1,36 @@
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('playlist_songs', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    playlist_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'playlists(id)',
+      referencesConstraintName: 'fk_playlist_songs.playlist_id_playlists.id',
+      onDelete: 'CASCADE',
+    },
+    song_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'songs(id)',
+      referencesConstraintName: 'fk_playlist_songs.songs_id_songs.id',
+      onDelete: 'CASCADE',
+    }
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable('playlist_songs');
+};
