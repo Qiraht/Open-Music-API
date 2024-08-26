19ef48dfc86c1f426a3138764cbba16ec6417816
Thariq <thariq2712@gmail.com>
2024-08-27 01:26:55 +0700

    add collaborations table migration

---
+++ migrations/1724648842124_create-table-collaborations.js
@@ -0,0 +1,38 @@
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('collaborations', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    playlist_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'playlists(id)',
+      referencesConstraintName: 'fk_collaborations.playlist_id_playlists.id',
+      onDelete: 'CASCADE',
+    },
+    user_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'users(id)',
+      referencesConstraintName: 'fk_collaborations.user_id_users.id',
+      onDelete: 'CASCADE',
+    },
+  });
+
+  pgm.addConstraint('collaborations', 'unique_playlist_id_and_user_id', 'UNIQUE(playlist_id, user_id)');
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable('authentications');
+};
