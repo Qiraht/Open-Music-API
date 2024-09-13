8f39e010063ddb2b314bcf26d4ee7e93d3e8748d
Thariq <thariq2712@gmail.com>
2024-09-13 21:59:52 +0700

    register export plugin

--- src/server.js
+++ src/server.js
@@ -30,6 +30,10 @@ const collaborations = require('./api/collaborations');
 const CollaborationsService = require('./services/postgres/CollaborationsService');
 const CollaborationsValidator = require('./validator/collaborations');
 
+const _exports = require('./api/exports');
+const ProducerService = require('./services/rabbitmq/ProducerService');
+const ExportsValidator = require('./validator/exports');
+
 const init = async () => {
   const albumsService = new AlbumsService();
   const songsService = new SongsService();
@@ -115,6 +119,14 @@ const init = async () => {
         collaborationsService,
         playlistsService,
         validator: CollaborationsValidator,
+      },
+    },
+    {
+      plugin: _exports,
+      options: {
+        service: ProducerService,
+        validator: ExportsValidator,
+        authorization: playlistsService,
       },
     },
   ]);
